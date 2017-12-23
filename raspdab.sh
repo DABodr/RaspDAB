c76bf2eccc94397960df440485e096e220a6a3ac
Lyonel <lyonel.bernard@gmail.com>
2017-12-23 19:31:50 +0100

    Update raspdab.sh
    La partie concernant ODR-DabMod a été décommenté.
    A recommenter pour utiliser ce script avec une carte EasyDAB v2

--- raspdab.sh
+++ raspdab.sh
@@ -169,14 +169,14 @@ sudo make install
 popd
 
 #COMMENTED OUT FOR RASPDAB
-#echo -e "$GREEN Compiling ODR-DabMod $NORMAL"
-#git clone https://github.com/Opendigitalradio/ODR-DabMod.git
-#pushd ODR-DabMod
-#./bootstrap.sh
-#./configure --enable-zeromq --enable-fft-simd
-#make
-#sudo make install
-#popd
+echo -e "$GREEN Compiling ODR-DabMod $NORMAL"
+git clone https://github.com/Opendigitalradio/ODR-DabMod.git
+pushd ODR-DabMod
+./bootstrap.sh
+./configure --enable-zeromq --enable-fft-simd
+make
+sudo make install
+popd
 
 echo -e "$GREEN Compiling fdk-aac library $NORMAL"
 git clone https://github.com/Opendigitalradio/fdk-aac.git
